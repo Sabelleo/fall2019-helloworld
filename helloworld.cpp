610d7f18bf304c4990293582d1e9959cd4f68c50
root <oconn177@mail.chapman.edu>
2019-09-05 04:08:14 +0000

    Minor edits

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+//Hello world assignment for CPSC 350
+using namespace std;
+
+//main method
+int main(int argc, char **argv) //argc?
+{
+    // output
+    cout << "Hello world" << endl;
+    return 0;
+}
